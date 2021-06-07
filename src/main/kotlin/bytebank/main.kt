d8062a4b675bea2e4c0ce0f59e8d9be95f0f6e03
Luiz Moraes <lffm1994@gmail.com>
2021-06-07 20:06:00 -0300

    Modulo 2 - Lambdas e funções anônimas com parâmetros e retorno

--- src/main/kotlin/bytebank/main.kt
+++ src/main/kotlin/bytebank/main.kt
@@ -21,8 +21,8 @@ package br.com.alura.bytebank
  */
 
 fun main() {
-    testaTipoFuncaoReferencia()
-    testaTipoFuncaoClasse()
+//    testaTipoFuncaoReferencia()
+//    testaTipoFuncaoClasse()
 
     /*
      2 Recursos para inicializar o tipo função
@@ -34,18 +34,42 @@ fun main() {
      nós colocamos uma função que seja compatível ao tipo que nós queremos,
      que seria uma função que não tem nenhum parâmetro e ela não retorna nada.
      Então isso é uma implementação de uma função anônima.
+
+     Detalhes para considerar ao utilizar expressão lambda
+     no momento que temos parametros e não utilizamos devemos renomear para _
+     isso significa que esse parametro não será utilizado só para bater a quantidade de
+     parametros
+
+     Então, o que seria de diferente?
+     Quando eu considero, ou não, a expressão lambda ou a função anônima?
+
+     A função anônima nós vamos considerar em situações nas quais nós queremos deixar
+     bem claro o que nós queremos colocar como parâmetro.
+     Como estamos vendo aqui, o que queremos colocar como retorno e também qual
+     o escopo de execução do código. Por quê? Porque aqui no escopo de execução
+     de código nós podemos executar o que quisermos, mas também somos obrigados
+     a falar quem retorna o quê. Ele não vai retornar a última expressão,
+     assim como nós vemos na expressão lambda.
+
+     Ele não retorna dessa maneira, ele retorna como se fosse uma função mesmo.
+     Então é dessa forma que funciona da função anônima.
+     Ela é uma maneira mais explícita de implementar uma expressão lambda.
+     Nós podemos entender dessa maneira.
      */
 
-//    val minhaFuncaoLambda =  {
-//        println("Executa como lambda")
-//    }
-//    println(minhaFuncaoLambda())
-//
-//
-//    val minhaFuncaoAnonima: () -> Unit = fun() {
-//        println("Executa como anonima")
-//    }
-//    println(minhaFuncaoAnonima())
+    //Inferir no tipo precisa deixar claro os valores do parametros e o tipo
+//    val minhaFuncaoLambda  =  {a: Int, b: Int ->
+    val minhaFuncaoLambda: (Int, Int) -> Int =  {a, b ->
+       a + b
+    }
+    println("Executa soma da função lambda: ${minhaFuncaoLambda(15,10)}")
+
+//    val minhaFuncaoAnonima: = fun(a: Int, b: Int): Int {
+    val minhaFuncaoAnonima: (Int, Int) -> Int = fun(a, b): Int {
+        println("Executa como anonima")
+        return a + b
+    }
+    println(minhaFuncaoAnonima(10, 2))
 }
 
 fun testaTipoFuncaoClasse() {
