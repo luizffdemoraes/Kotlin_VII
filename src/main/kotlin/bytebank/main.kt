53b7da3edf7f1b128839f55547b5dfb7a9db8c67
Luiz Moraes <lffm1994@gmail.com>
2021-06-08 01:12:15 -0300

    Modulo 5 - Testando o run

--- src/main/kotlin/bytebank/main.kt
+++ src/main/kotlin/bytebank/main.kt
@@ -1,12 +1,52 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaPoupanca
 import br.com.alura.bytebank.modelo.Endereco
 
 
 fun main() {
+    testaRun()
 
-    testaWith()
 
+}
+
+fun testaRun() {
+    val taxaAnual = 0.05
+    val taxaMensal: Double = taxaAnual / 12
+    println("taxa mensal $taxaMensal")
+
+    val novaConta1 = ContaPoupanca(Cliente(nome = "Alex", cpf = "111.111.111-11", senha = 1234), 1000)
+    novaConta1.run {
+        deposita(1000.0)
+        println(saldo)
+        saldo * taxaMensal
+    }.let { rendimentoMensal ->
+        println(rendimentoMensal)
+    }
+
+
+    val novaConta2 = Cliente(nome = "Luiz", cpf = "222.222.222-22", senha = 1234)
+        .let { clienteNovo: Cliente ->
+            ContaPoupanca(clienteNovo, 1000)
+        }.run {
+            deposita(2000.0)
+            println(saldo)
+            saldo * taxaMensal
+        }.let { rendimentoMensal ->
+            println("rendimento mensal $rendimentoMensal")
+        }
+
+    //calculo de juros compostos
+    val rendimentoAnual = run {
+        var saldo: Double = novaConta1.saldo
+        repeat(12) {
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+
+    println("simulacao rendimento mensal $rendimentoAnual")
 }
 
 fun testaWith() {
